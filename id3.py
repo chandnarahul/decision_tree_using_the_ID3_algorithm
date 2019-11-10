a957d0a61c91a7dbfa036c3a282ce2936fa1c598
Rahul Chandna <rahul.chandna@investec.co.za>
2019-11-10 22:13:31 +0200

    refactored to use pandas to reduce complexity

--- id3.py
+++ id3.py
@@ -1,41 +1,20 @@
 from math import log
-from typing import Dict
 
-inputFile = open("car.csv", "r")
+import pandas as pd
 
-columnsAndTheirCountMap: Dict[int, Dict[str, int]] = {}
-totalNumberOfRowsInFile = 0
-lengthOfColumns = 0
-for line in inputFile.readlines():
-    totalNumberOfRowsInFile = totalNumberOfRowsInFile + 1
-    dataAsArray = line.strip().split(',')
+dataFrame = pd.read_csv("car.csv", header=None)
 
-    lengthOfColumns = len(dataAsArray)
+totalRowsInDataSet = dataFrame.shape[0]
+print('Total number of lines in the file [' + str(totalRowsInDataSet) + ']\n')
 
-    for labelIndex in range(lengthOfColumns):
-        labelColumnValue = dataAsArray[labelIndex]
-        if labelIndex in columnsAndTheirCountMap:
-            innerDictionary: Dict[str, int] = columnsAndTheirCountMap[labelIndex]
-            try:
-                innerDictionary[labelColumnValue] = innerDictionary[labelColumnValue] + 1
-            except KeyError:
-                innerDictionary[labelColumnValue] = 1
+lastColumnInDataSet = len(dataFrame.columns) - 1
 
-            columnsAndTheirCountMap[labelIndex] = innerDictionary
-        else:
-            innerDictionary: Dict[str, int] = {labelColumnValue: 1}
-            columnsAndTheirCountMap[labelIndex] = innerDictionary
-
-print('All columns and their label types: \n' + str(columnsAndTheirCountMap) + '\n')
-
-decisionsAndTheirCountMap = columnsAndTheirCountMap[lengthOfColumns - 1]
-print('Total number of lines in the file [' + str(totalNumberOfRowsInFile) + ']\n')
-
-print('Decisions and their count: \n' + str(decisionsAndTheirCountMap) + '\n')
+decisionsAndCount = dataFrame[lastColumnInDataSet].value_counts()
 
 entropy = 0
-for (decision, count) in decisionsAndTheirCountMap.items():
-    p = (count / totalNumberOfRowsInFile)
-    entropy = entropy - (p * log(p, len(decisionsAndTheirCountMap)))
+numberOfDecisions = len(decisionsAndCount);
+for decisionCount in range(numberOfDecisions):
+    p = decisionsAndCount[decisionCount] / totalRowsInDataSet
+    entropy = entropy - (p * log(p, numberOfDecisions))
 
 print('Entropy for decisions is [' + str(entropy) + ']\n')
